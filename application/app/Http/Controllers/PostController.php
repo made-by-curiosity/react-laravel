9cf35f1f35a21535a80021e502241cab2fb007e0
Curiosity <agnkvdm@gmail.com>
2025-07-09 11:41:02 +0300

    feat: add crud operations for posts

---
+++ application/app/Http/Controllers/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostStoreRequest;
+use App\Http\Requests\PostUpdateRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Auth::user()->posts()->latest()->get();
+
+        return response()->json([
+            'posts' => $posts,
+        ], Response::HTTP_OK);
+    }
+
+    public  function store (PostStoreRequest $request)
+    {
+        $post = Auth::user()->posts()->create($request->validated());
+
+        return response()->json([
+            'post' => $post,
+        ], Response::HTTP_CREATED);
+    }
+
+    public  function show (int $id)
+    {
+        try {
+            $post = Auth::user()->posts()->findOrFail($id);
+
+            return response()->json([
+                'post' => $post,
+            ], Response::HTTP_OK);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Post not found.',
+            ], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public  function update (PostUpdateRequest $request, int $id)
+    {
+        try {
+            $post = Auth::user()->posts()->findOrFail($id);
+            $post->update($request->validated());
+
+            return response()->json([
+                'post' => $post,
+            ]);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Post not found.',
+            ], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public  function destroy ($id)
+    {
+        try {
+            $post = Auth::user()->posts()->findOrFail($id);
+            $post->delete();
+
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Post not found.',
+            ], Response::HTTP_NOT_FOUND);
+        }
+    }
+}
